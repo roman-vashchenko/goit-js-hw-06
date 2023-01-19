7010ba4f68b15b995e1bb2e7ab212d03e0107840
Roman Vashchenko <roman1991dp@gmail.com>
2023-01-20 01:18:02 +0200

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,13 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const btn = document.querySelector('.change-color');
+const textColor = document.querySelector('.change-color');
+
+btn.addEventListener('click', onChangeColorBtnClick);
+
+function onChangeColorBtnClick() {
+  document.body.style.backgroundColor = getRandomHexColor();
+  textColor.textContent = document.body.style.backgroundColor;
+}
