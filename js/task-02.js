acddbf656338f31eb52b355640b869667aaa1ebe
Roman Vashchenko <roman1991dp@gmail.com>
2023-01-19 13:48:22 +0200

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,10 @@
-const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
-];
+const ingredients = ['Potatoes', 'Mushrooms', 'Garlic', 'Tomatos', 'Herbs', 'Condiments'];
+
+const ulElement = document.querySelector('#ingredients');
+
+for (const ingredient of ingredients) {
+  const ulItem = document.createElement('li');
+  ulItem.textContent = ingredient;
+  ulItem.classList.add('item');
+  ulElement.appendChild(ulItem);
+}
