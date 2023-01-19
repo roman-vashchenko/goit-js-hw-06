f9b643bcd523cc12c63870c1e46ec9af9af82622
Roman Vashchenko <roman1991dp@gmail.com>
2023-01-19 23:52:15 +0200

    Update task-06.js

--- js/task-06.js
+++ js/task-06.js
@@ -1,13 +1,13 @@
 const input = document.querySelector('#validation-input');
+const numberСharacters = Number(input.dataset.length);
 
 input.addEventListener('blur', onInputBlur);
 
 function onInputBlur(event) {
-  if (event.currentTarget.value.length >= input.dataset.length) {
+  if (event.currentTarget.value.length === numberСharacters) {
     input.classList.add('valid');
     input.classList.remove('invalid');
-  }
-  if (event.currentTarget.value.length < input.dataset.length) {
+  } else {
     input.classList.remove('valid');
     input.classList.add('invalid');
   }
